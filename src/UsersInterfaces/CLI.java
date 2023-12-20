62ba7423512aa670686b396e417bda6e8b2bb52d
Ян <145141197+YanSokolovsky@users.noreply.github.com>
2023-12-20 20:40:50 +0300

    To stast CLI implementation.

--- src/UsersInterfaces/CLI.java
+++ src/UsersInterfaces/CLI.java
@@ -1,5 +1,34 @@
 package UsersInterfaces;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class CLI {
+    // command "cd filename"
+    // command "encode filename"
+    // command "decode filename"
+    // command "addtoarch filename"
+    // command "cleartoarch"
+    // command "rar rarfile"
+    // command "zip zipfile"
+    //
+    boolean isExist(String fileName) {
+        ArrayList<String> files = getListOfFiles();
+        return files.contains(currPath + "\\" + fileName);
+    }
     String currPath;
+    ArrayList<String> getListOfFiles() {
+        File folder = new File(currPath);
+        File[] files = folder.listFiles();
+        ArrayList<String> ListOfNames = new ArrayList<>();
+        if (files == null) {
+            return null;
+        } else {
+            for (File file : files) {
+                ListOfNames.add(file.getName());
+            }
+        }
+        return ListOfNames;
+    }
+
 }
